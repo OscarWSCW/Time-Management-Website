677523358772051802d8ffa5424458592fbbd3d4
OscarWSCW <159733064+OscarWSCW@users.noreply.github.com>
2025-10-19 19:58:10 +1300

    quiz, benefits and finishing touches

--- script.js
+++ script.js
@@ -2,28 +2,89 @@
 
 // Questions
 const questions = [
-  {
-    question: "What is the best way to start managing your time?",
+   {
+    question: "What is Time Management?",
     answers: [
-      { text: "Ignore tasks", correct: false },
-      { text: "Planning", correct: true },
-      { text: "Procrastinate", correct: false },
-      { text: "Do everything at once", correct: false },
+      { text: "How you spend your time", correct: false },
+      { text: "How to waste your time", correct: false },
+      { text: "What to do with your time", correct: false },
+      { text: "The process of organizing your time to increase efficency and productivity", correct: true },
     ]
   },
   {
-    question: "Which tool is commonly used to schedule tasks?",
+    question: "What is the best way to start managing your time?",
     answers: [
-      { text: "Calendar", correct: true },
-      { text: "Dice", correct: false },
-      { text: "Random guess", correct: false },
-      { text: "Nothing", correct: false },
+      { text: "Ignore your tasks", correct: false },
+      { text: "Plan them", correct: true },
+      { text: "Procrastinate them", correct: false },
+      { text: "Multitask", correct: false },
+    ]
+  },
+  {
+    question: "Which tool is can be used to schedule tasks?",
+    answers: [
+      { text: "using a physical calendar", correct: false },
+      { text: "Using online apps such as google calander", correct: false },
+      { text: "Writing them on Paper", correct: false },
+      { text: "All of the above", correct: true },
+    ]
+  },
+   {
+    question: "What are some of the benefits of time management?",
+    answers: [
+      { text: "Make you more lazy", correct: false },
+      { text: "More missed deadlines", correct: false },
+      { text: "decreased stress levels", correct: true },
+      { text: "There are no benefits", correct: false },
+    ]
+  },
+   {
+    question: "True or False: Time management can help improve focus",
+    answers: [
+      { text: "True", correct: true },
+      { text: "False", correct: false },
+     
+    ]
+  },
+   {
+    question: "True or False: Time management only applies to teenagers",
+    answers: [
+      { text: "False", correct: true },
+      { text: "True", correct: false },
+     
+    ]
+  },
+   {
+    question: "What does having bad time management mainly do?",
+    answers: [
+      { text: "increases productivity", correct: false },
+      { text: "Decreased Stress", correct: false },
+      { text: "Increased Stress", correct: true },
+      { text: "None of the above", correct: false },
+    ]
+  },
+   {
+    question: "What ages does bad time management commonly affect the most?",
+    answers: [
+      { text: "High school students", correct: true },
+      { text: "Adults", correct: false },
+      { text: "University students", correct: true },
+      { text: "All of the above", correct: false },
+    ]
+  },
+   {
+    question: "What percentage of students have stress from bad time management?",
+    answers: [
+      { text: "28%", correct: false },
+      { text: "55%", correct: true },
+      { text: "78%", correct: false },
+      { text: "60%", correct: false },
     ]
   },
   {
     question: "Why is time management important?",
     answers: [
-      { text: "It reduces stress", correct: true },
+      { text: "It helps you hand in tasks earlier", correct: true },
       { text: "It makes you procrastinate more", correct: false },
       { text: "It wastes time", correct: false },
       { text: "It makes you less productive", correct: false },
